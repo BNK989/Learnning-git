58f81d6f7d6d58cd58d255fd4cad5420eed96025
Ben Klinovsky <bnk989@gmail.com>
2024-02-13 15:09:41 +0200

    Add random to ball functionality

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+'use strict'
 let heightWidth = 100
 
 function onBallClick(elBall){
     if(heightWidth >= 400) heightWidth = 50
-    heightWidth += 50
+    heightWidth += getRandomInt(20,61)
     elBall.style.width = heightWidth + 'px'
     elBall.style.height = heightWidth + 'px'
     elBall.innerText = heightWidth
