ef20aaae680dd3eb0b27a55fcd6593e9890f0ce8
Ben Klinovsky <bnk989@gmail.com>
2024-02-13 15:48:10 +0200

    add separate control to each ball

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 'use strict'
-let heightWidth = 100
 
 function onBallClick(elBall){
-    if(heightWidth >= 400) heightWidth = 50
-    heightWidth += getRandomInt(20,61)
-    elBall.style.width = heightWidth + 'px'
-    elBall.style.height = heightWidth + 'px'
-    elBall.innerText = heightWidth
+    const maxDiameter = 400
+    const rand = getRandomInt(20,61)
+    let currSize = +elBall.style.width.slice(0,-2)
+    if(currSize <= 100) currSize = 100
+    if(currSize >= maxDiameter){
+        elBall.style.width = '100px'
+        elBall.style.height = '100px'
+        elBall.innerText = 100
+    } else {
+        elBall.style.width = currSize + rand + 'px'
+        elBall.style.height = currSize + rand + 'px'
+        elBall.innerText = currSize + rand
+    }
     elBall.style.backgroundColor = getRandomColor()
 }
