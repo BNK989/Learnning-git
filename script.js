d5df0e4563c6ebd7fe745cc8891d6e294d88ddb8
Ben Klinovsky <bnk989@gmail.com>
2024-02-13 14:34:24 +0200

    Change js functionality to limit size to 400

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let heightWidth = 100
 
 function onBallClick(elBall){
+    if(heightWidth >= 400) heightWidth = 50
     heightWidth += 50
     elBall.style.width = heightWidth + 'px'
     elBall.style.height = heightWidth + 'px'
+    elBall.innerText = heightWidth
 }
